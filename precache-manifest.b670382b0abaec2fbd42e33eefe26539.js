773cb500d0f60b421d2477bbb9c12778c2d9e12b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-23 04:12:33 +0000

    Deploy vaj90/COMP3104 to github.com/vaj90/COMP3104.git:gh-pages

---
+++ precache-manifest.b670382b0abaec2fbd42e33eefe26539.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ff9b03331fd092c92c4",
+    "url": "/vaj90/COMP3104/static/css/main.6b1b2876.chunk.css"
+  },
+  {
+    "revision": "9ff9b03331fd092c92c4",
+    "url": "/vaj90/COMP3104/static/js/main.9ff9b033.chunk.js"
+  },
+  {
+    "revision": "c41b44fe323d7657aaa0",
+    "url": "/vaj90/COMP3104/static/js/runtime~main.c41b44fe.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/vaj90/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "d7a787de601137865f7b07fa9a0fecc8",
+    "url": "/vaj90/COMP3104/index.html"
+  }
+];
